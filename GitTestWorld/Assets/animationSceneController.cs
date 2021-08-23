87ff1bfd14a634a19fd630e14c606f5ca9306125
Misho Metodiev <mishman3004@gmail.com>
2021-08-22 21:34:26 -0500

    Animations functioning

---
+++ GitTestWorld/Assets/animationSceneController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationSceneController : MonoBehaviour
+{
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isWalking = animator.GetBool("isWalking");
+        bool isRunning = animator.GetBool("isRunning");
+        bool walkPressed = Input.GetKey("w");
+        bool runPressed = Input.GetKey("left shift");
+
+        if(!isWalking && walkPressed && !runPressed) {
+            animator.SetBool("isWalking", true);
+            animator.SetBool("isRunning", false);
+        }
+        if(!isRunning && runPressed) {
+            animator.SetBool("isRunning", true);
+            animator.SetBool("isWalking", false);
+        }
+        if(!walkPressed) {
+            animator.SetBool("isWalking", false);
+            animator.SetBool("isRunning", false);
+        }
+    }
+}
